2ceab74d0398f0ab290e85a20e574cb0de29c09e
Akshaya Salunkhe <akshaya.salunkhe@example.com>
2025-09-14 19:12:49 +0530

    hashset city code added here

---
+++ DemoProjectJava/src/com/velocity/HashSetDemo.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	
+	public static void main(String[] args) {
+		
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("Pune");
+		hs.add("Mumbai");
+		hs.add("Delhi");
+		hs.add("Chennai");
+		
+		System.out.println(hs);
+	}
+
+}
