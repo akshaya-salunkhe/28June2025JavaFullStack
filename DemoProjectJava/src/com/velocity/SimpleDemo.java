b17e64d2b6f6e2c7434ea066443044a40a63777a
Akshaya Salunkhe <akshaya.salunkhe@example.com>
2025-09-14 19:27:34 +0530

    Java simple program added here

---
+++ DemoProjectJava/src/com/velocity/SimpleDemo.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class SimpleDemo {
+	
+	public static void main(String args[]) {
+		
+		String s = "This is java language program";
+		
+		System.out.println(s);
+	}
+
+}
