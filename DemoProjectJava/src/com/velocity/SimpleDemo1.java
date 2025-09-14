a7a418df200a1dee5ec17ce73f27d57aafc49a0d
Akshaya Salunkhe <akshaya.salunkhe@example.com>
2025-09-14 19:33:39 +0530

    simple demo 1 added here

---
+++ DemoProjectJava/src/com/velocity/SimpleDemo1.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class SimpleDemo1 {
+	
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 30;
+		int c = a+b;
+		
+		System.out.println("Addition = " + c);
+	}
+
+}
