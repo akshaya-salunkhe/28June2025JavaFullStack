7c8ffa57de3db5b949b5d7fabc9f661ba0889f6f
Akshaya Salunkhe <akshaya.salunkhe@example.com>
2025-09-14 23:52:03 +0530

    subtraction added into code

--- DemoProjectJava/src/com/velocity/SimpleDemo1.java
+++ DemoProjectJava/src/com/velocity/SimpleDemo1.java
@@ -6,9 +6,10 @@ public class SimpleDemo1 {
 		
 		int a = 20;
 		int b = 30;
-		int c = a+b;
-		
-		System.out.println("Addition = " + c);
+		//int c = a+b;
+		int d = b-a;
+		//System.out.println("Addition = " + c);
+		System.out.println("Subtraction = " + d);
 	}
 
 }
